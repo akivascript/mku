2e50ecb101b4ac14fc9415aeb7b51ffae42c6bfe
Akiva <akiva@akivamail.com>
2018-03-28 15:21:57 -0500

    Latest changes to pacer.

--- src/reckoner/pacer.clj
+++ src/reckoner/pacer.clj
@@ -1,32 +1,83 @@
-(ns recokoner.pacer
-  (:require [clojure.spec.alpha :as s]))
+(ns reckoner.pacer
+  (:require [clojure.spec.alpha :as s]
+            [reckoner.hero.arythea :as ary]))
 
-(s/def ::crystal #{:black :gold :blue :green :red :white})
+(s/def ::count (s/and int? pos?))
+(s/def ::crystal #{:blue :green :red :white})
+(s/def ::crystals (s/keys :req [::crystal
+                                ::count]))
+(s/def ::number (s/and pos? #(<= % 16)))
+(s/def ::deed (s/keys :req [::number
+                            ::crystal]))
+(s/def ::deck (s/and vector? (s/coll-of ::deed)))
 (s/def ::hero #{:arythea :goldyx :norowas :tovak :wolfhawk})
-(s/def ::order (s/and int? pos?))
-(s/def ::card (s/and pos? #(<= % 16)))
-(s/def ::deeds (s/and set? (s/coll-of ::card)))
+(s/def ::order (s/and int? (not neg?)))
+(s/def ::tactic (s/and int? #(<= % 6)))
 (s/def ::turn (s/and pos? #(<= % 6)))
 
-(def dummy
+(s/def ::pacer (s/keys :req [::hero
+                             ::crystals
+                             ::deeds
+                             ::order
+                             ::tactic]))
+
+
+(def hero
+  #{:arythea :goldyx :norowas :tovak :wolfhawk})
+
+(def pacer
   {::hero nil
-   ::crystals nil
-   ::turn 1
-   ::cards (into #{} (mapv inc (range 16)))
-   })
+   ::crystals []
+   ::deck nil
+   ::order 0
+   ::tactic 0})
+
+(defn make-deck
+  [deck]
+  (into (sorted-map) (zipmap (map inc (range)) deck)))
+
+(def arythea
+  (assoc! (transient pacer)
+          ::hero :arythea
+          ::crystals ary/crystals
+          ::deck (make-deck ary/deck)
+          ::order 0
+          ::tactic 0))
+
+(def game
+  {::turn 1})
 
 (defn tactic
-  [d]
-  (update d ::turn #(inc (rand-int 6))))
+  [p]
+  (update p ::turn #(inc (rand-int 6))))
 
 (defn end-turn
-  [d]
+  [p]
 
   )
 
+(defn draw
+  [deck]
+  (let [deck (if-not (nil? (some keyword? (keys deck)))
+               (:deck deck)
+               deck)
+        card (loop [n (inc (rand-int (count deck)))]
+               (if-not (nil? (get deck n))
+                 n
+                 (recur (inc (rand-int (count deck))))))]
+    {:color (get deck card)
+     :deck (dissoc deck card)}))
+
 (defn play
-  [d]
-  (when (empty? (::cards d))
-    (end-turn d))
-  (mapi)
-  ) 
+  [p]
+  (when (empty? (::deck p))
+    (end-turn p))
+  (let [{color :color deck :deck} (last (take 4 (iterate draw deck)))]
+
+    )) 
+
+(defn init
+  ([]
+   (init (rand-nth (seq hero))))
+  ([hero]
+   (persistent! (assoc! dummy ::hero ))))
